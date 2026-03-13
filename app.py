d408f666634ce07f74a2749d6b3262a32d8fa470
root <root@ip-10-192-187-7.eu-north-1.compute.internal>
2026-03-13 11:28:13 +0000

    pytn

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>🚀 Nocturnal Animal</h1>
+    <p>Successfully deployed using Docker & Jenkins CI/CD Pipeline!</p>
+    """
+
+@app.route("/health")
+def health():
+    return {"status": "running"}, 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
